c1e0ae6beee515f3ac756133a0f591736a7042a6
Isai Castro <isaicstr@gmail.com>
2022-11-08 21:38:35 -0700

    Working on infinite logo scroller

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import ScrollingLogo from "./components/scrolling-logo/scrolling-logo.component";
+
+const App = () => {
+  return (
+    <div className="App">
+      <ScrollingLogo />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/scrolling-logo/scrolling-logo.component.js
@@ -0,0 +1,86 @@
+import "./scrolling-logo.styles.scss";
+
+const ScrollingLogo = () => {
+  return (
+    <div className="scrolling-logo-container">
+      <div className="marquee-container">
+        <div className="marquee">
+          <div className="marquee-inner">
+            <div className="logo">
+              <img src="/assets/html.png" alt="html" />
+            </div>
+            <div className="logo">
+              <img src="assets/css.png" alt="css" />
+            </div>
+            <div className="logo">
+              <img src="/assets/javascript.png" alt="javascript" />
+            </div>
+            <div className="logo">
+              <img src="/assets/react.png" alt="react" />
+            </div>
+            <div className="logo">
+              <img src="/assets/node-js.webp" alt="node" />
+            </div>
+            <div className="logo">
+              <img src="/assets/express-logo.png" alt="express" />
+            </div>
+            <div className="logo">
+              <img src="/assets/postgresql-logo.png" alt="postgresql" />
+            </div>
+            <div className="logo">
+              <img src="/assets/git.png" alt="git" />
+            </div>
+            <div className="logo">
+              <img src="/assets/firebase.png" alt="firebase" />
+            </div>
+            <div className="logo">
+              <img src="/assets/postman.png" alt="postman" />
+            </div>
+            <div className="logo">
+              <img src="/assets/npm.png" alt="npm" />
+            </div>
+          </div>
+        </div>
+        <div className="marquee">
+          <div className="marquee-inner">
+            <div className="logo">
+              <img src="/assets/html.png" alt="html" />
+            </div>
+            <div className="logo">
+              <img src="assets/css.png" alt="css" />
+            </div>
+            <div className="logo">
+              <img src="/assets/javascript.png" alt="javascript" />
+            </div>
+            <div className="logo">
+              <img src="/assets/react.png" alt="react" />
+            </div>
+            <div className="logo">
+              <img src="/assets/node-js.webp" alt="node" />
+            </div>
+            <div className="logo">
+              <img src="/assets/express.png" alt="express" />
+            </div>
+            <div className="logo">
+              <img src="/assets/postgresql.png" alt="postgresql" />
+            </div>
+            <div className="logo">
+              <img src="/assets/git.png" alt="git" />
+            </div>
+            <div className="logo">
+              <img src="/assets/firebase.png" alt="firebase" />
+            </div>
+            <div className="logo">
+              <img src="/assets/postman.png" alt="postman" />
+            </div>
+            <div className="logo">
+              <img src="/assets/npm.png" alt="npm" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollingLogo;
